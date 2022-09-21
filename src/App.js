011d23694fa9597501cefc4eb004e73356160138
cmj1980 <dagoclmt@gmail.com>
2022-09-21 10:33:22 -0700

    deps and base styling done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './Components/Layout/index'
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
