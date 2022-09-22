b6a001d31058abc51f496f58d028174b400c0d9a
cmj1980 <dagoclmt@gmail.com>
2022-09-21 18:16:02 -0700

    added navlinks and icons on sidebar

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -1,14 +1,33 @@
 import './index.scss'
-import myLogo from '../../assests/images/myLogo.png'
+import { Link, NavLink } from 'react-router-dom'
+import Logo from '../../assests/images/mylogo.png'
+import SubLogo from '../../assests/images/sublogo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCode, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
-const Sidebar = () => {
-  return (
-    <div className="nav-bar">
-      <link className="logo" to="/">
-        <img src={myLogo} alt="logo" />
-      </link>
-    </div>
-  )
-}
+const Sidebar = () => (
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={Logo} alt="logo" />
+      <img className="sub-logo" src={SubLogo} alt="sublogo" />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink exact="true" activeclassname="active" to="/mywork">
+        <FontAwesomeIcon icon={faCode} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+  </div>
+)
 
 export default Sidebar
