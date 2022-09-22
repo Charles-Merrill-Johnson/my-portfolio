deb6ac5fe7564e3cb6fa613ce9677448ff2ceb54
cmj1980 <dagoclmt@gmail.com>
2022-09-22 12:22:10 -0700

    added social media links

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -9,6 +9,7 @@ import {
   faHome,
   faUser,
 } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -45,6 +46,26 @@ const Sidebar = () => (
         <FontAwesomeIcon icon={faFile} color="#4d4d4e" />
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/charles-merrill-johnson/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/Charles-Merrill-Johnson"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 )
 
