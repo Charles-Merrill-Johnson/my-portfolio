2436f45f035d8f6ef57eb9ad9dd1bc5c8bc3fab2
cmj1980 <dagoclmt@gmail.com>
2022-09-25 17:35:16 -0700

    added state to side bar

--- src/Components/Sidebar/sidebar.js
+++ src/Components/Sidebar/sidebar.js
@@ -4,65 +4,75 @@ import Logo from '../../assests/images/mylogo.png'
 import SubLogo from '../../assests/images/sublogo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import React, { useState } from 'react'
+import { ReactDOM } from 'react-dom'
 
-const Sidebar = () => (
-  <div className="nav-bar">
-    <Link className="logo" to="/">
-      <img src={Logo} alt="logo" />
-      <img className="sub-logo" src={SubLogo} alt="sublogo" />
-    </Link>
-    <nav>
-      <NavLink exact="true" activeclassname="active" to="/">
-        HOME
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="about-link"
-        to="/about"
-      >
-        ABOUT
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="mywork-link"
-        to="/mywork"
-      >
-        CODE
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="files-link"
-        to="/files"
-      >
-        FILES
-      </NavLink>
-    </nav>
-    <ul>
-      <li>
-        <a
-          target="_blank"
-          className="anchor-icon"
-          rel="noreferrer"
-          href="https://www.linkedin.com/in/charles-merrill-johnson/"
+const Sidebar = () => {
+  const [toggleOpen, setToggleOpen] = useState(false)
+
+  const ToggleSidebar = () => {
+    toggleOpen === true ? setToggleOpen(false) : setToggleOpen(true)
+  }
+
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={Logo} alt="logo" />
+        <img className="sub-logo" src={SubLogo} alt="sublogo" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          HOME
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
         >
-          <FontAwesomeIcon icon={faLinkedin} color="e2dbd2" />
-        </a>
-      </li>
-      <li>
-        <a
-          target="_blank"
-          className="anchor-icon"
-          rel="noreferrer"
-          href="https://github.com/Charles-Merrill-Johnson"
+          ABOUT
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="mywork-link"
+          to="/mywork"
         >
-          <FontAwesomeIcon icon={faGithub} color="e2dbd2" />
-        </a>
-      </li>
-    </ul>
-  </div>
-)
+          CODE
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="files-link"
+          to="/files"
+        >
+          FILES
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            className="anchor-icon"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/charles-merrill-johnson/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="e2dbd2" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            className="anchor-icon"
+            rel="noreferrer"
+            href="https://github.com/Charles-Merrill-Johnson"
+          >
+            <FontAwesomeIcon icon={faGithub} color="e2dbd2" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
 
 export default Sidebar
